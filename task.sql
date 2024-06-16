37bcfa6c70e28ea039d23de576b6140b6be6d6c2
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-06-16 18:44:02 +0300

    Solution_v1.2_name of index updated to Email on rollback statement

--- task.sql
+++ task.sql
@@ -51,4 +51,4 @@ CREATE TABLE Users (
 
 --changeset yevhenii.rozdorozhnii:6 labels:0.0.3
 CREATE INDEX Email ON Users (Email);
---rollback ALTER TABLE Users DROP INDEX idx_Email;
+--rollback ALTER TABLE Users DROP INDEX Email;
